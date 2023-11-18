37c0b7c16dcfba8a713f19eac3383a72431c9d15
nquidox <nquidox@gmail.com>
2023-11-18 23:00:52 +0300

    post form with axios

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,23 @@
 <script>
+import axios from "axios";
+
 export default {
   name: 'App',
 
   data(){
     return{
-      username: '',
-      password: '',
+      form: {
+        username: '',
+        password: ''
+      }
     }
   },
 
   methods: {
     login(){
-      const requestOptions = {
-        method: 'POST',
-        headers: {
-          // 'Content-Type': 'application/json',
-          'Content-Type': 'application/x-www-form-urlencoded',
-          'Access-Control-Allow-Origin': 'http://localhost:8000',
-          'Access-Control-Allow-Methods': '*',
-          'Access-Control-Allow-Credentials': 'true',
-          'Access-Control-Allow-Headers': 'Content-Type, Authorization'
-        },
-        body: JSON.stringify({grant_type: '', username: this.username, password: this.password, scope: '', client_id: '', client_secret: ''})
-      };
-
-      fetch('http://127.0.0.1:8000/token', requestOptions)
-          .then(response => console.log(response))
-    }
+      const auth = `grant_type=&username=${this.form.username}&password=${this.form.password}&scope=&client_id=&client_secret=`
+      axios.post("https://127.0.0.1:8000/token", auth).then(response => console.log(response));
+    },
   },
 }
 </script>
@@ -35,20 +26,20 @@ export default {
   <div class="container">
     <div class="row justify-content-center">
       <div class="col-4 m-3">
-        <form>
+        <form id="login_form">
           <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
 
           <div class="form-floating">
-            <input v-bind:value="username" type="text" class="form-control" id="floatingInput" placeholder="username">
+            <input v-model="form.username" type="text" class="form-control" id="floatingInput" placeholder="username">
             <label for="floatingInput">Username</label>
           </div>
 
           <div class="form-floating">
-            <input v-bind:value="password" type="password" class="form-control" id="floatingPassword" placeholder="Password">
+            <input v-model="form.password" type="password" class="form-control" id="floatingPassword" placeholder="Password">
             <label for="floatingPassword">Password</label>
           </div>
 
-          <button class="btn btn-primary w-100 py-2" @click="login">Sign in</button>
+          <button class="btn btn-primary w-100 py-2" @click="login" type="button">Sign in</button>
           <p class="mt-5 mb-3 text-body-secondary">&copy; 2023</p>
         </form>
       </div>
